cc0f887a3ae2c8e2a88011746206c09c671dc5d0
ShashiSingh72 <shashisingh223344@gmail.com>
2025-10-04 08:06:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "rg13" {
-  name = "dev-rg82"
+resource "azurerm_resource_group" "rg11" {
+  name = "dev-rg8"
   location = "eastus"
 }
-resource "azurerm_resource_group" "rg14" {
-  name = "dev-rg83" 
+resource "azurerm_resource_group" "rg12" {
+  name = "dev-rg9" 
   location = "eastus"
 }
